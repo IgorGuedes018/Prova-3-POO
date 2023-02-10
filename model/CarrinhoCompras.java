a35c9589418bb28e344d7caea8e0efe0bf0752de
Igor Almeida Guedes <104040747+IgorGuedes018@users.noreply.github.com>
2023-02-10 20:49:36 -0300

    prova3 final

--- model/CarrinhoCompras.java
+++ model/CarrinhoCompras.java
@@ -7,16 +7,27 @@ public class CarrinhoCompras {
 	private Pedido pedido;
 	private ArrayList<ItensCompra> itemCompra;
 	
+	public Pedido getPedido() {
+		return pedido;
+	}
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+	public ArrayList<ItensCompra> getItemCompra() {
+		return itemCompra;
+	}
+	public void setItemCompra(ArrayList<ItensCompra> itemCompra) {
+		this.itemCompra = itemCompra;
+	}
 	
 	
-	
-	
-	
-	public void incluirItens() {	
+	public void incluirItens() {
+		
 	}
 	public ArrayList<Produto>  buscarProduto(){
 		ArrayList<Produto> p = new ArrayList<>();
 		return p;
 	}
+	
 
 }
